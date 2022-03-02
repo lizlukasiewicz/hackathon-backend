7ed69f5e941a2f202aad4f12cc85e7db3f8395cc
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2022-03-01 20:03:00 -0500

    rendering names from json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState, useEffect } from 'react'
+//import Attendees from './components/Attendees';
+// import {
+//   BrowserRouter as Router,
+//   Route
+// } from 'react-router-dom'
+
 
 function App() {
+  const [people, setPeople]= useState([])
+  const getData=() => {
+    fetch('attendees.json', {
+      headers : {
+        'Content-Type': 'application/json', 
+        'Accept' : 'application/json'
+      }
+    }
+    )
+    .then((response) => response.json())
+    .then((json) => setPeople(json))
+    .catch((err) =>{
+      console.log(err)
+    })
+  }
+  useEffect(()=> {
+    getData()
+  }, [])
+  const peopleList = people?.map((person, index) => {
+    return(
+      <li key={index}>
+        <p> {person.name} </p>
+      </li>
+    )
+  })
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to Nerd Out!</h1>
       </header>
+      <ul className="attendeeList">
+        {peopleList}
+      </ul>
     </div>
   );
 }
 
 export default App;
+//<Attendees people={people}/>
+// .then((myJson) => {
+//   console.log(myJson)
+//   setPeople(myJson)
+// })
---
+++ src/components/Attendees.js
@@ -0,0 +1,11 @@
+//import {Link} from 'react-router-dom'
+
+export default function Attendees(props) {
+    console.log(props)
+    
+    return(
+        <div>
+            <p>this is the attendees component</p>
+        </div>
+    )
+}
